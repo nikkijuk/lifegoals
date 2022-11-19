c8713b3ae69c86a5b6c7d6118e69e72318dd1134
Jukka Nikki <jukka.nikki@kolumbus.fi>
2022-11-19 19:46:55 +0100

    code coverage issues

--- lib/core/navigation.dart
+++ lib/core/navigation.dart
@@ -1,19 +1,13 @@
-import 'package:equatable/equatable.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lifegoals/about/view/about_page.dart';
 import 'package:lifegoals/counter/counter.dart';
 
-class Routes extends Equatable {
+class Routes {
   static const home = '/';
-
   static const about = '/about';
-
-  @override
-  List<Object?> get props => [home, about];
 }
 
-GoRouter router([String? initialLocation]) => GoRouter(
-      initialLocation: initialLocation ?? Routes.home,
+GoRouter router() => GoRouter(
       routes: [
         GoRoute(
           path: Routes.home,
