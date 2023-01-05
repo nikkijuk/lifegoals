dbfc9ad4fed78f0cb6b68720025a20d543cc7fe6
Jukka Nikki <jukka.nikki@kolumbus.fi>
2023-01-05 11:54:34 +0100

    lint rule fix

--- lib/features/counter/view/counter_page.dart
+++ lib/features/counter/view/counter_page.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lifegoals/core/navigation.dart';
+import 'package:lifegoals/domain/authentication/authenticated_user.dart';
 import 'package:lifegoals/features/authentication/bloc/authentication_bloc.dart';
 import 'package:lifegoals/features/authentication/bloc/authentication_state.dart';
 import 'package:lifegoals/features/counter/cubit/counter_cubit.dart';
 import 'package:lifegoals/l10n/l10n.dart';
-
-import 'package:lifegoals/domain/authentication/authenticated_user.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
